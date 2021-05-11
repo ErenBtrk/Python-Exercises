81b7f4a2bc3288ff896a9b161d97fa531336bbe8
ErenBtrk <erenbtrk@hotmail.com>
2021-05-11 16:13:03 +0300

    Exercise 21

---
+++ Exercise21.py
@@ -0,0 +1,25 @@
+'''
+Take the code from the How To Decode A Website exercise 
+(if you didn’t do it or just want to play with some different code, use the code from the solution),
+and instead of printing the results to a screen, write the results to a txt file. In your code, 
+just make up a name for the file you are saving to.
+
+Extras:
+
+Ask the user to specify the name of the output file that will be saved.
+
+'''
+
+import random
+
+fileName = input("Please enter the file name : ")
+
+number_list = [random.randrange(1, 100, 1) for i in range(10)]
+number_list_str = [ str(item) for item in number_list]
+print(number_list_str)
+
+with open(fileName,"w") as file:
+    for item in number_list_str:
+        file.write(item+' ')
+
+
---
+++ Exercise22.py
@@ -0,0 +1,35 @@
+'''
+    Given a .txt file that has a list of a bunch of names, 
+    count how many of each name there are in the file, 
+    and print out the results to the screen. 
+
+Extra:
+
+    Instead of using the .txt file from above (or instead of, if you want the challenge),
+    take this .txt file, and count how many of each “category” of each image there are. 
+    This text file is actually a list of files corresponding to the SUN database scene 
+    recognition database, and lists the file directory hierarchy for the images. 
+    Once you take a look at the first line or two of the file, it will be clear which
+    part represents the scene category. To do this, you’re going to have to remember
+    a bit about string parsing in Python 3. I talked a little bit about it in this post.
+    
+'''
+
+file = open("example.txt","r")
+count = 0
+countDarth = 0
+countLea = 0
+
+for item in file:
+    count += 1
+    if(item == "Darth\n"):
+        countDarth += 1
+    if(item == "Lea\n"):
+        countLea += 1
+
+print(count)
+print(countDarth)
+print(countLea)
+
+
+
