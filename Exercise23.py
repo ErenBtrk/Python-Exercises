7ce3e26150bb25394a024df5723c2cc6feab752c
ErenBtrk <erenbtrk@hotmail.com>
2021-05-13 15:28:00 +0300

    Exercise 23

--- Exercise23.py
+++ Exercise23.py
@@ -1,31 +1,34 @@
 '''
-    take this .txt file, and count how many of each “category” of each image there are. 
-    This text file is actually a list of files corresponding to the SUN database scene 
-    recognition database, and lists the file directory hierarchy for the images. 
-    Once you take a look at the first line or two of the file, it will be clear which
-    part represents the scene category. To do this, you’re going to have to remember
-    a bit about string parsing in Python 3. I talked a little bit about it in this post.
+ Given two .txt files that have lists of numbers in them, find the numbers that are overlapping.
+ One .txt file has a list of all prime numbers under 1000, and the other .txt file has a list of 
+ happy numbers up to 1000.
+ 
+ (If you forgot, prime numbers are numbers that can’t be divided by any other number.
+ And yes, happy numbers are a real thing in mathematics - you can look it up on Wikipedia.
+ The explanation is easier with an example, which I will describe below.)
 '''
 
-dict1 = {}
-dict2 = {}
-count = 0
+with open("happy.txt") as happyFile:
+    happyLine = happyFile.readline()
+    happyList = []
+    while happyLine:
+        happyList.append(happyLine.strip())
+        happyLine = happyFile.readline()
 
-with open("images.txt") as file:
-    line = file.readline()
-    while line:
-        count += 1
-        str_list = line.strip().split('/')
-        #print(str_list)
-        #print(line)
-        if(str_list[1] in dict1):
-            dict1[str_list[1]] += 1
-        else:
-            dict1[str_list[1]] = 1
-        line = file.readline()
-        if(str_list[2] in dict2):
-            dict2[str_list[2]] += 1
-        else:
-            dict2[str_list[2]] = 1
-print(dict1)
-print(dict2)
+happyFile.close()
+            
+with open("prime.txt") as primeFile:
+    primeLine = primeFile.readline()
+    primeList = []
+    while primeLine:
+        primeList.append(primeLine.strip())
+        primeLine = primeFile.readline()
+
+primeFile.close()
+
+for item in happyList:
+    if(item in primeList):
+        print(item)
+
+
+
